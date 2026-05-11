80b623d03631a9bba961d80d5419a358162f8994
Deloney-code <simedelonneynjeba@gmail.com>
2026-05-11 19:34:25 +0100

    Fix static files warning - only include static dir if it exists

--- core/settings.py
+++ core/settings.py
@@ -95,7 +95,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
